d1e47382c76c371312f111c0174639d795231eee
Lee Salminen <leesalminen@gmail.com>
2023-06-12 08:00:37 -0600

    asdf

--- index.js
+++ index.js
@@ -63,12 +63,8 @@ app.use(bodyParser.json({
 
 // HMAC verification middleware
 app.use((req, res, next) => {
+  console.log(req.url.split("?")[0])
   if(noAuthPaths.indexOf(req.url.split("?")[0]) !== -1) {
-    next()
-    return
-  }
-
-  if(req.url.split("?")[0].startsWith('/tipSplit')) {
     next()
     return
   }
